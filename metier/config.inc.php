940979356146ef22d11649a56fb1e94eb902febd
Nicole <gingrasnic@gmail.com>
2015-03-30 16:40:59 -0400

    config

--- metier/config.inc.php
+++ metier/config.inc.php
@@ -1,5 +1,5 @@
 <?php
-	define('DSN','mysql:host=nicolegingras.ca;dbname=nicolegi_artsvisuels');
-	define('USAGER','nicolegi_gingni');
-	define('MOTPASSE','^OktqziEGV,!');
+	define('DSN','mysql:host=localhost;dbname=nicolegi_artsvisuels');
+	define('USAGER','root');
+	define('MOTPASSE','');
 	define('BASEDONNEES','nicolegi_artsvisuels');
